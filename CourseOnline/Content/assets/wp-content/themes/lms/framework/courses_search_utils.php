3951f79d765874182f251dafbeebde4fe009ca0d
My PC <tvngiagia@gmail.com>
2020-01-24 00:28:18 +0700

    Update giao dien.

---
+++ CourseOnline/Content/assets/wp-content/themes/lms/framework/courses_search_utils.php
@@ -0,0 +1,79 @@
+<span id="dt-course-search-datas" data-postperpage="10" data-curr_page="1" data-offset="0" data-costtype="all" data-searchtext="" data-coursetype="0" data-subcoursetype="0" data-webinar="" style="display:none;"></span><div class="dt-sc-clear"></div>
+	  <div class="dt-sc-hr-invisible-medium"></div><table border="0" cellpadding="0" class="courses-table-list tablesorter">
+			<thead>
+			  <tr>
+				<th class="courses-table-title-header" scope="col">Course Name</th>
+				<th class="courses-table-type-header" scope="col">Course Type</th>
+				<th class="courses-table-lessons-header" scope="col">Lessons #</th>
+				<th class="courses-table-cost-header" scope="col">Cost</th>
+				<th class="courses-table-length-header" scope="col">Length</th>
+			  </tr>
+			</thead>
+			<tbody><tr>
+				<td class="courses-table-title"><a href="https://lmstheme.wpengine.com/courses/power-electronics/">Power Electronics</a></td>
+				<td class="courses-table-type"><a href="https://lmstheme.wpengine.com/coursecategory/education/">Education</a></td>
+				<td class="courses-table-lessons">6</td>
+				<td class="courses-table-cost">$50</td>
+				<td class="courses-table-length">5 hours 35 mins </td>
+			  </tr><tr>
+				<td class="courses-table-title"><a href="https://lmstheme.wpengine.com/courses/introduction-to-calculus/">Introduction to Calculus</a></td>
+				<td class="courses-table-type"><a href="https://lmstheme.wpengine.com/coursecategory/mathematics/">Mathematics</a></td>
+				<td class="courses-table-lessons">5</td>
+				<td class="courses-table-cost">Free</td>
+				<td class="courses-table-length">3 hours 15 mins </td>
+			  </tr><tr>
+				<td class="courses-table-title"><a href="https://lmstheme.wpengine.com/courses/basic-laws-and-policies/">Basic Laws and Policies</a></td>
+				<td class="courses-table-type"><a href="https://lmstheme.wpengine.com/coursecategory/law/">Law</a></td>
+				<td class="courses-table-lessons">5</td>
+				<td class="courses-table-cost">$40</td>
+				<td class="courses-table-length">5 hours 10 mins </td>
+			  </tr><tr>
+				<td class="courses-table-title"><a href="https://lmstheme.wpengine.com/courses/healthcare-delivery/">Healthcare Delivery</a></td>
+				<td class="courses-table-type"><a href="https://lmstheme.wpengine.com/coursecategory/health/">Health</a></td>
+				<td class="courses-table-lessons">4</td>
+				<td class="courses-table-cost">$50</td>
+				<td class="courses-table-length">3 hours 25 mins </td>
+			  </tr><tr>
+				<td class="courses-table-title"><a href="https://lmstheme.wpengine.com/courses/general-business-law/">General Business Law</a></td>
+				<td class="courses-table-type"><a href="https://lmstheme.wpengine.com/coursecategory/law/">Law</a></td>
+				<td class="courses-table-lessons">7</td>
+				<td class="courses-table-cost">Free</td>
+				<td class="courses-table-length">6 hours 5 mins </td>
+			  </tr><tr>
+				<td class="courses-table-title"><a href="https://lmstheme.wpengine.com/courses/emerging-trends-and-technologies/">Emerging Trends and Technologies</a></td>
+				<td class="courses-table-type"><a href="https://lmstheme.wpengine.com/coursecategory/engineering/">Engineering</a></td>
+				<td class="courses-table-lessons">4</td>
+				<td class="courses-table-cost">Free</td>
+				<td class="courses-table-length">3 hours 10 mins </td>
+			  </tr><tr>
+				<td class="courses-table-title"><a href="https://lmstheme.wpengine.com/courses/fitness-training-program/">Fitness Training Program</a></td>
+				<td class="courses-table-type"><a href="https://lmstheme.wpengine.com/coursecategory/health/">Health</a></td>
+				<td class="courses-table-lessons">6</td>
+				<td class="courses-table-cost">$40</td>
+				<td class="courses-table-length">4 hours 25 mins </td>
+			  </tr><tr>
+				<td class="courses-table-title"><a href="https://lmstheme.wpengine.com/courses/architecture-analysis-design/">Architecture Analysis &#038; Design</a></td>
+				<td class="courses-table-type"><a href="https://lmstheme.wpengine.com/coursecategory/engineering/">Engineering</a></td>
+				<td class="courses-table-lessons">5</td>
+				<td class="courses-table-cost">$30</td>
+				<td class="courses-table-length">4 hours 15 mins </td>
+			  </tr><tr>
+				<td class="courses-table-title"><a href="https://lmstheme.wpengine.com/courses/maintenance-training/">Maintenance Training</a></td>
+				<td class="courses-table-type"><a href="https://lmstheme.wpengine.com/coursecategory/engineering/">Engineering</a></td>
+				<td class="courses-table-lessons">2</td>
+				<td class="courses-table-cost">$60</td>
+				<td class="courses-table-length">1 hour 30 mins </td>
+			  </tr><tr>
+				<td class="courses-table-title"><a href="https://lmstheme.wpengine.com/courses/analysis-of-algorithms/">Analysis of Algorithms</a></td>
+				<td class="courses-table-type"><a href="https://lmstheme.wpengine.com/coursecategory/mathematics/">Mathematics</a></td>
+				<td class="courses-table-lessons">6</td>
+				<td class="courses-table-cost">Free</td>
+				<td class="courses-table-length">5 hours 20 mins </td>
+			  </tr></tbody></table><div class='pagination' data-postid=''><ul><li class='active-page'>1</li><li><a href='#'>2</a></li></ul><div class="next-post"><a href="#" cpage="1"  class="dt-next">Next <span class="fa fa-angle-double-right"></span></a></div></div>
+<script type="text/javascript">
+			jQuery(document).ready(function($){
+				if ($(".courses-table-list").length > 0) {
+					$(".courses-table-list").tablesorter();
+				}	
+			});
+		  </script>
